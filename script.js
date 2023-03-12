2fddf87d7818a959053a6d9e9c0385980972130a
Prakrisht Dahiya <63427355+beetrandahiya@users.noreply.github.com>
2023-03-13 02:06:15 +0530

    added a lil kinda nice transition

--- script.js
+++ script.js
@@ -8,3 +8,25 @@ var bg_letter = document.querySelector('.bg-letter');
 bg_letter.setAttribute('x', window.innerWidth / 2-5*window.innerWidth/100);
 bg_letter.setAttribute('y', window.innerHeight / 2+10*window.innerHeight/100);
 
+
+// hide description-1 and reveal the .description-2 element gradually as mouse moves to the right
+var description_2 = document.querySelector('.description-2');
+var description_1 = document.querySelector('.description-1');
+
+window.addEventListener('mousemove', function(e) {
+    if(e.clientX<window.innerWidth/2-window.innerWidth/4){
+        description_1.style.opacity = 1;
+        description_2.style.opacity = 0;
+    }
+    else if(e.clientX>window.innerWidth/2+window.innerWidth/4){
+        description_1.style.opacity = 0;
+        description_2.style.opacity = 1;
+    }
+    else{
+    var x = (e.clientX - window.innerWidth/2+window.innerWidth/4) / (window.innerWidth/4);
+    description_1.style.opacity = 1 - x;
+    description_2.style.opacity = x;
+    }
+    });
+
+
